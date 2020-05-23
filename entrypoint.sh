d3fc0020738d9ad0c7f0ada6e3bbb6d9e3284d3a
Vince Tse <vincetse@users.noreply.github.com>
2020-05-23 15:12:54 +0000

    Add Python and custom commands

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,19 @@ set -e
 # relative path of directory to build
 APP_SUBDIR=${APP_SUBDIR:=.}
 
+# set up the directory location to the mounted app
 export IMPORT_PATH=/github/workspace/${APP_SUBDIR}
-/bin/herokuish buildpack test
+
+# customize the build and test commands if the respective
+# environment variables are defined.
+if [[ -n "${BUILD_COMMAND}" ]]; then
+  ${BUILD_COMMAND}
+else
+  /bin/herokuish buildpack build
+fi
+
+if [[ -n "${TEST_COMMAND}" ]]; then
+  ${TEST_COMMAND}
+else
+  /bin/herokuish buildpack test
+fi
