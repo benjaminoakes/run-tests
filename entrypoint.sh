6f4e5ba65393cb8c981d70b715f97e4e2bc860c9
Vince Tse <vincetse@users.noreply.github.com>
2020-05-23 19:45:42 +0000

    Add comments

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 
-# relative path of directory to build
+# relative path of directory to build, i.e. the root of the
+# application that we want to build.
 APP_SUBDIR=${APP_SUBDIR:=.}
 
 # set up the directory location to the mounted app
